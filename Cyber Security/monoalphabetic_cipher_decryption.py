9f509108d5003815cdab41e9c7ccfd0395cabf91
Chasmic Coder <chasmiccoder@gmail.com>
2020-12-30 12:03:56 +0530

    Started on Cipher problem, and report

---
+++ Cyber Security/monoalphabetic_cipher_decryption.py
@@ -0,0 +1,10 @@
+"""
+We need to decrypt the following string:
+ndgzixzgvtnozvydgtvbvdinooczrjjyviynojizjacdggcjpnzviyrcvozqzmrvgfzyoczmzrvgfzyvgjiz 
+
+Decrypted Text:
+
+Given Information:
+Cipher used = Monoalphabetic Cipher
+
+"""
